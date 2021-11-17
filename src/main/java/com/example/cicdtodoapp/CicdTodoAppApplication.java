2abb07bb725c78d3d7ffd3508c929ec31341ef2e
bguiang <bernardguiang@gmail.com>
2021-11-16 20:35:58 -0700

    initial commit

---
+++ src/main/java/com/example/cicdtodoapp/CicdTodoAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.cicdtodoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdTodoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdTodoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cicdtodoapp/CicdTodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cicdtodoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdTodoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
