a609a55625fb0cc001b7a03825a668ebc194e096
bguiang <bernardguiang@gmail.com>
2021-11-17 20:43:28 -0700

    TDD: actually there's no need to include a database yet. We can generate the id's manually with the list size

--- src/main/java/com/example/cicdtodoapp/models/Task.java
+++ src/main/java/com/example/cicdtodoapp/models/Task.java
@@ -1,13 +1,30 @@
 package com.example.cicdtodoapp.models;
 
+//@Entity
 public class Task {
 
+//	@Id
+//	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
 	private String name;
 	
 	public Task() {}
 	
 	public Task(String name) {
 		this.name = name;
+	}
+	
+	public Task(String name, long id) {
+		this.id = id;
+		this.name = name;
+	}
+	
+	public long getId() {
+		return id;
+	}
+	
+	public void setId(long id) {
+		this.id = id;
 	}
 
 	public String getName() {
